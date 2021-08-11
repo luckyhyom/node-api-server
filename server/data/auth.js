7d1a34c3acc5eb01670ab0337cb4dd3b2871e6f5
luckyhyom <bs_khm@naver.com>
2021-08-11 21:33:59 +0900

    메시지 mysql로 변경 (crud)

--- server/data/auth.js
+++ server/data/auth.js
@@ -1,22 +1,22 @@
 import {db} from '../db/database.js';
 
 export async function findByUsername(username) {
-    return users.find((user) => user.username === username);
+    return db
+    .execute('SELECT * FROM users WHERE username=?',[username])
+    .then(result => result[0][0]);
 }
 
 export async function findById(id) {
-      const result = users.find((user) => user.id === id);
-    return result;
+    return db
+    .execute('SELECT * FROM users WHERE id=?', [ id ])
+    .then(result => result[0][0]);
 }
 
 export async function createUser(user) {
     const {username, password, name, email, url} = user;
-    return db.excute(
+    return db.execute(
         'INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)',
         [username, password, name, email, url]
     )
-    .then((result) => {
-        console.log(result);
-        return result;
-    })
+    .then((result) => result[0].insertId);
 }
--- server/data/tweet.js
+++ server/data/tweet.js
@@ -6,55 +6,38 @@
  * username,email,등은 userRepository에서 가지고 있으니 userId라는 해당 데이터를 가리키는 키만 가지고있도록 수정한다.
 */ 
 
+import { db } from '../db/database.js';
 import * as userRepository from './auth.js';
 
+const SELECT_JOIN = 'SELECT tw.id, tw.text, tw.userId, tw.createdAt, us.username, us.name, us.url FROM tweets as tw JOIN users as us ON tw.userId=us.id';
+const ORDER_DESC = 'ORDER BY tw.createdAt DESC';
+
 export async function getAll() {
-  return Promise.all(
-    tweets.map(async (tweet) => {
-      const { username, name, url } = await userRepository.findById(
-        tweet.userId
-      );
-      return { ...tweet, username, name, url };
-    })
-  );
+  return db
+  .execute(`${SELECT_JOIN} ${ORDER_DESC}`)
+  .then(result => console.log(result[0]));
 }
 
 export async function getAllByUsername(username) {
-  return getAll().then((tweets) =>
-    tweets.filter((tweet) => tweet.username === username)
-  );
+  return db
+  .execute( `${SELECT_JOIN} WHERE us.username=? ${ORDER_DESC}`, [ usernmae ] )
+  .then(result => console.log(result[0]))
 }
 
 export async function getById(id) {
-  const found = tweets.find((tweet) => tweet.id === id);
-  if (!found) {
-    return null;
-  }
-  const { username, name, url } = await userRepository.findById(found.userId);
-  return { ...found, username, name, url };
+  return db.execute(`${SELECT_JOIN} WHERE tw.id=?`, [ id ]).then(result=> result[0][0]);
 }
 
 export async function create(text, userId) {
-  const tweet = {
-    id: Date.now().toString(),
-    text,
-    createdAt: new Date(),
-    userId,
-  };
-
-  tweets = [tweet, ...tweets];
-
-  return getById(tweet.id);
+  return db.execute('INSERT INTO tweet.tweets (text, createdAt, userId) VALUES (?,?,?)', [ text, new Date(), userId ])
+  .then(result => getById(result[0].insertId));
 }
 
 export async function update(id, text) {
-  const tweet = tweets.find(tweet => tweet.id === id);
-  if(tweet) {
-    tweet.text = text;
-  }
-  return getById(tweet.id);
+  return db.execute('UPDATE tweets SET text=? WHERE id=?', [ text, id ])
+  .then(() => getById(id));
 }
 
 export async function remove(id) {
-  tweets = tweets.filter(tweet => tweet.id !== id);
+  return db.execute('DELETE FROM tweets WHERE id=?', [ id ]);
 }
