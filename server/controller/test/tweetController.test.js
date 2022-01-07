5278d9f7b76d17175287d6bc2e00b372a4c4ae3e
luckyhyom <bs_khm@naver.com>
2022-01-07 22:33:34 +0900

    [Test] tweetController 테스트코드 리팩토링
    - 모든 것이 객체 기반인 자바스크립트의 특징을 이용하여 빈 객체를 넣으므로써 모의한다.
    - 반복되는 조건들을 줄이므로써 핵심 로직 파악이 쉬워진다.
    - 호출할 의존성의 api를 정의한다.
    - 의존성이 반환해야할 값을 정의한다.

--- server/controller/test/tweetController.test.js
+++ server/controller/test/tweetController.test.js
@@ -1,153 +1,164 @@
 import { TweetController } from '../tweet.js';
-import { StubTweetRepository } from './stubTweetRepository.js';
-import { stubGetSocketIO } from './stubSocketIO';
-import httpMocks from 'node-mocks-http'
+import httpMocks from 'node-mocks-http';
+import faker from 'faker';
+// faker
+// toMatchObject
+// repository = {} , repository.getById = () => {...} or jest..
+// 위에서 repo를 주입하고 아래에서 repo를 수정한다는 점
+// 지금 repo는 너무 구체적이다, 상황만 알수있으면 된다.
 
 describe('tweet Controller', () => {
     let controller;
+    let tweetsRepository;
+    let socket;
     beforeEach(() => {
-        controller = new TweetController(new StubTweetRepository(), stubGetSocketIO);
+        tweetsRepository = {};
+        socket = { emit: jest.fn() };
+        controller = new TweetController(tweetsRepository, () => socket);
     })
 
     describe('create', () => {
-        it('생성', async () => {
-            let req = httpMocks.createRequest({
-                body: {
-                    text: '텍스트 내용'
-                },
-                userId: '유저 아이디'
+        let newTweet, authorId, req, res;
+        beforeEach(() => {
+            newTweet = faker.random.words(3);
+            authorId = faker.random.alphaNumeric(16);
+            req = httpMocks.createRequest({
+                body: { text: newTweet },
+                userId: authorId,
             });
-            let res = httpMocks.createResponse();
+            res = httpMocks.createResponse();
+        })
+        it('생성', async () => {
+            tweetsRepository.create = jest.fn((text, userId) => ({
+                text,
+                userId,
+            }));
 
             await controller.create(req, res);
 
             expect(res.statusCode).toBe(201);
-            expect(res._getJSONData()).toEqual({
-                id: expect.any(Number),
-                userId: '유저 아이디',
-                text: '텍스트 내용',
+            expect(res._getJSONData()).toMatchObject({
+                text: newTweet,
+                userId: authorId,
             });
-        })
+            expect(tweetsRepository.create).toHaveBeenCalledWith(newTweet, authorId);
+        });
+
+        it('웹소켓 이벤트 발생', async () => {
+            tweetsRepository.create = jest.fn((text, userId) => ({
+                text: text,
+                userId: userId,
+            }));
+    
+            await controller.create(req, res);
+    
+            expect(socket.emit).toHaveBeenCalledWith('tweets', {
+                text: newTweet,
+                userId: authorId,
+            });
+        });
     })
-    describe('get', () => {
-        it('저장된 트윗이 없다면?', () => {
-            
-        })
+    describe('getTweets', () => {
         it('유저 이름의 트윗 반환', async () => {
-            let req = httpMocks.createRequest({
-                query: {
-                    username: '효민',
-                },
+            const username = faker.internet.userName();
+            const req = httpMocks.createRequest({
+                query: { username },
             });
-            let res = httpMocks.createResponse();
+            const res = httpMocks.createResponse();
+            const userTweets = [{ text: faker.random.words(3) }];
+            tweetsRepository.getAllByUsername = () => userTweets;
 
             await controller.getTweets(req, res);
 
-            expect(res._getJSONData()).toEqual([{
-                id: 1,
-                text: 'one..',
-                username: '효민',
-                userId: '효민 아이디'
-            }]);
+            expect(res.statusCode).toBe(200);
+            expect(res._getJSONData()).toMatchObject(userTweets);
         })
+
+        /**
+         * repository가 무엇을 반환하는지 상세하게 알 필요 없다.
+         * 하지만 반환받은 값에 무언가 속성을 추가해야 할 때는 컨트롤러에서 검증해야한다.
+         */
         it('전달받은 유저 이름이 없다면 모든 트윗을 반환', async () => {
-            let req = httpMocks.createRequest();
-            let res = httpMocks.createResponse();
+            const req = httpMocks.createRequest();
+            const res = httpMocks.createResponse();
+            const allTweets = [
+                { text: faker.random.words(3) },
+                { text: faker.random.words(3) }
+            ]
+            tweetsRepository.getAll = () => allTweets;
 
             await controller.getTweets(req, res);
             
-            expect(res._getJSONData()).toEqual([
-                {
-                    id: 1,
-                    text: 'one..',
-                    username: '효민',
-                    userId: '효민 아이디'
-                },
-                {
-                    id: 2,
-                    text: 'two..',
-                    username: '짱구',
-                    userId: '짱구 아이디'
-                },
-                {
-                    id: 3,
-                    text: 'three..',
-                    username: '철수',
-                    userId: '철수 아이디'
-                },
-            ]);
+            expect(res.statusCode).toBe(200);
+            expect(res._getJSONData()).toMatchObject(allTweets);
         });
 
         describe('트윗 아이디로 찾기', () => {
+            let tweetId, req, res;
+
+            beforeEach(() => {
+                tweetId = faker.random.alphaNumeric(16);
+                req = httpMocks.createRequest({
+                    params: { id: tweetId },
+                });
+                res = httpMocks.createResponse();
+            })
             it('트윗 아이디가 존재하는 경우', async () => {
-                let req = httpMocks.createRequest({
-                    params: {
-                        id: 1
-                    }
-                });
-                let res = httpMocks.createResponse();
+                const tweet = faker.random.words(3);
+                tweetsRepository.getById = jest.fn(() => tweet);
 
                 await controller.getById(req, res);
                 
-                expect(res._getJSONData()).toEqual(
-                    {
-                        id: 1,
-                        text: 'one..',
-                        username: '효민',
-                        userId: '효민 아이디'
-                    }
-                );
+                expect(res.statusCode).toBe(200);
+                expect(res._getJSONData()).toEqual(tweet);
+                expect(tweetsRepository.getById).toHaveBeenCalledWith(tweetId);
             });
+
             it('트윗 아이디가 존재하지 않는 경우', async () => {
-                let req = httpMocks.createRequest({
-                    params: {
-                        id: 100
-                    }
-                });
-                let res = httpMocks.createResponse();
+                tweetsRepository.getById = jest.fn(() => undefined);
 
                 await controller.getById(req, res);
                 
-                expect(res._getJSONData().message).toBe('Tweet id(100) not found');
+                expect(res.statusCode).toBe(404);
+                expect(res._getJSONData().message).toBe(`Tweet id(${tweetId}) not found`);
+                expect(tweetsRepository.getById).toHaveBeenCalledWith(tweetId);
+                
             });
         })
     });
 
     describe('update', () => {
-        it('해당 아이디의 트윗 텍스트 변경', async () => {
-            let req = httpMocks.createRequest({
-                params: {
-                    id: 1
-                },
-                body: {
-                    text: '수정된 텍스트'
-                },
-                userId: '효민 아이디'
+        let tweetId, updatedText, req, res, authorId;
+        beforeEach(() => {
+            tweetId = faker.random.alphaNumeric(16);
+            updatedText = faker.random.words(3);
+            authorId = faker.random.alphaNumeric(16);
+            req = httpMocks.createRequest({
+                params: { id: tweetId },
+                body: { text: updatedText },
+                userId: authorId,
             });
-            let res = httpMocks.createResponse();
+            res = httpMocks.createResponse();
+        });
+        it('해당 아이디의 트윗 텍스트 변경', async () => {
+            tweetsRepository.getById = () => ({
+                text: faker.random.words(3),
+                userId: authorId,
+            });
+            tweetsRepository.update = (tweetId, newText) => ({
+                text: newText,
+            });
 
             await controller.update(req, res);
-            
-            expect(res._getJSONData()).toEqual(
-                {
-                    id: 1,
-                    text: '수정된 텍스트',
-                    username: '효민',
-                    userId: '효민 아이디'
-                }
-            );
+
+            expect(res.statusCode).toBe(200);
+            expect(res._getJSONData()).toMatchObject({
+                text: updatedText,
+            });
         });
         it('해당 아이디의 트윗이 없을 때 404 에러 반환', async () => {
-            let req = httpMocks.createRequest({
-                params: {
-                    id: 100
-                },
-                body: {
-                    text: '수정된 텍스트'
-                },
-                userId: "효민 아이디"
-            });
-            let res = httpMocks.createResponse();
+            tweetsRepository.getById = () => undefined;
+            tweetsRepository.update = jest.fn();
 
             await controller.update(req, res);
             
@@ -155,17 +166,10 @@ describe('tweet Controller', () => {
         });
 
         it('아이디가 일치하지 않을 때 403 에러', async () => {
-            let req = httpMocks.createRequest({
-                params: {
-                    id: 1
-                },
-                body: {
-                    text: '수정된 텍스트'
-                },
-                userId: "철수 아이디"
+            tweetsRepository.getById = () => ({
+                userId: faker.internet.userName(),
             });
-            let res = httpMocks.createResponse();
-
+            
             await controller.update(req, res);
             
             expect(res.statusCode).toBe(403);
@@ -174,18 +178,49 @@ describe('tweet Controller', () => {
     })
 
     describe('remove', () => {
-        it('트윗 삭제 성공시 204 반환', async () => {
-            let req = httpMocks.createRequest({
-                params: {
-                    id: 1
-                },
-                userId: '효민 아이디'
+        let tweetId, req, res, authorId;
+        beforeEach(() => {
+            tweetId = faker.random.alphaNumeric(128);
+            authorId = faker.internet.userName();
+            req = httpMocks.createRequest({
+                params: { id: tweetId },
+                userId: authorId
             });
-            let res = httpMocks.createResponse();
+            res = httpMocks.createResponse();
+        });
+        it('트윗 삭제 성공시 204 반환', async () => {
+            tweetsRepository.getById = () => ({
+                id: tweetId,
+                userId: authorId,
+            });
+
+            // 호출 할 의존성 api를 명시한다.
+            tweetsRepository.remove = jest.fn();
+
+            await controller.remove(req, res);
+
+            expect(res.statusCode).toBe(204);
+        });
+        it('userId가 일치하지 않을 경우 403 반환', async () => {
+            tweetsRepository.getById = () => ({
+                id: tweetId,
+                userId: faker.random.alphaNumeric(16),
+            });
+            tweetsRepository.remove = jest.fn();
 
             await controller.remove(req, res);
             
-            expect(res.statusCode).toBe(204);
+            expect(res.statusCode).toBe(403);
+        });
+
+        it('트윗이 없다면 404 반환', async () => {
+            tweetsRepository.getById = () => undefined;
+            tweetsRepository.remove = jest.fn();
+
+            await controller.remove(req, res);
+
+            expect(res.statusCode).toBe(404);
+            expect(tweetsRepository.remove).not.toHaveBeenCalled();
         });
     })
 })
